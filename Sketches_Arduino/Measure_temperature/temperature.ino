21008d07b05387c41b89a8e7eae4fa2f93171803
leandro.goetzer <leandro@goetzer.ch>
2021-10-11 15:23:34 +0200

    Temperature Messung erstellt - Struktur angepasst

---
+++ Sketches_Arduino/Measure_temperature/temperature.ino
@@ -0,0 +1,39 @@
+/*
+ MKR ENV Shield - Temperature Read Sensor
+ */
+
+#include <Arduino_MKRENV.h>
+
+void setup() {
+   Serial.begin(9600);
+
+  while (!Serial);
+
+  if (!ENV.begin()) {
+
+    Serial.println("Failed to initialize MKR ENV shield!");
+
+    while (1);
+    
+  }
+}
+
+void loop() {
+  
+  // read all the sensor values
+  
+    float temperature = ENV.readTemperature();
+  
+  // print each of the sensor values
+  
+    Serial.print("Temperature = ");
+    Serial.print(temperature);
+  
+   // print an empty line
+
+    Serial.println();
+
+  // wait 1 second to print again
+
+    delay(1000);
+}
