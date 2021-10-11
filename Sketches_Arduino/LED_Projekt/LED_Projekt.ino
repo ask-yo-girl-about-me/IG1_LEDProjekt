d8889a10211daf3aae0a47e8cbc54b9012f23992
leandro.goetzer <leandro@goetzer.ch>
2021-10-11 14:16:35 +0200

    code angepasst

--- Sketches_Arduino/LED_Projekt/LED_Projekt.ino
+++ Sketches_Arduino/LED_Projekt/LED_Projekt.ino
@@ -1,7 +1,7 @@
 #include "FastLED.h"
-#define ANZAHL_LEDS 60
-#define LED_PIN 6
 
+#define ANZAHL_LEDS 60
+#define LED_PIN 10
 CRGB leds[ANZAHL_LEDS];
 
 void setup() {
@@ -9,10 +9,19 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < ANZAHL_LEDS; i++) {
-    leds[i] = CRGB::Green;
-    FastLED.show();
-    leds[i] = CRGB::Black;
-    delay(200);
-  }
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Blue;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Green;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Yellow;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(1000);
 }
