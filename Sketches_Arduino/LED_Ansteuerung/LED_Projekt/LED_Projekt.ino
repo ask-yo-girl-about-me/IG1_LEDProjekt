99445de049406e574d5009932dbf4e14c7383b60
gshmeidig <mschmidli@gmx.net>
2021-10-28 21:37:06 +0200

    LED Sketch

--- Sketches_Arduino/LED_Ansteuerung/LED_Projekt/LED_Projekt.ino
+++ Sketches_Arduino/LED_Ansteuerung/LED_Projekt/LED_Projekt.ino
@@ -1,5 +1,5 @@
 #include "FastLED.h"
-#define NUM_LEDS 60
+#define NUM_LEDS 40
 
 CRGB leds[NUM_LEDS];
 void setup() { FastLED.addLeds<NEOPIXEL,  13>(leds, NUM_LEDS); }
@@ -7,16 +7,16 @@ void loop() {
   leds[0] = CRGB::Red;
   FastLED.show();
   delay(1000);
-  leds[0] = CRGB::Blue;
+  leds[1] = CRGB::Blue;
   FastLED.show();
   delay(1000);
-  leds[0] = CRGB::Green;
+  leds[2] = CRGB::Green;
   FastLED.show();
   delay(1000);
-  leds[0] = CRGB::Yellow;
+  leds[3] = CRGB::Yellow;
   FastLED.show();
   delay(1000);
-  leds[0] = CRGB::Black;
+  leds[40] = CRGB::Black;
   FastLED.show();
   delay(1000);
 }
