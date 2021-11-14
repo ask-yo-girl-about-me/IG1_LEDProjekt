29c4f88371e7da4abc4d76840407835e36680c6d
leandro.goetzer <leandro@goetzer.ch>
2021-11-14 16:05:34 +0100

    Sketches angepasst

---
+++ 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/LED_Ansteuerung/LED-Einzeln/LED-Einzeln.ino
@@ -0,0 +1,26 @@
+void setup() {
+  pinMode(A1, OUTPUT);
+  digitalWrite(A1, HIGH);
+    pinMode(A2, OUTPUT);
+  digitalWrite(A2, HIGH);
+    pinMode(A3, OUTPUT);
+  digitalWrite(A3, HIGH);
+    pinMode(A4, OUTPUT);
+  digitalWrite(A4, HIGH);
+    pinMode(A5, OUTPUT);
+  digitalWrite(A5, HIGH);
+    pinMode(A6, OUTPUT);
+  digitalWrite(A6, HIGH);
+    pinMode(0, OUTPUT);
+  digitalWrite(0, HIGH);
+      pinMode(1, OUTPUT);
+  digitalWrite(1, HIGH);
+      pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+      pinMode(3, OUTPUT);
+
+  
+}
+void loop(){
+}
