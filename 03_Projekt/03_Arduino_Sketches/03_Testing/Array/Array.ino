1b1d4426227b73f1d0efb66c3772b7779f1ab6df
gshmeidig <mschmidli@gmx.net>
2021-11-25 20:10:10 +0100

    .

---
+++ 03_Projekt/03_Arduino_Sketches/03_Testing/Array/Array.ino
@@ -0,0 +1,24 @@
+
+
+void setup() {
+const LED1=0,
+const LED2=1,
+const LED3=2,
+const LED4=3
+const LED5=4,
+const LED6=5,
+const LED7=6,
+const LED8=7,
+const LED9=8,
+const LED10=9,
+array [LED1,LED2,LED3,LED4,LED5,LED6,LED7,LED8,LED9,LED10]
+for (i=0, i<9, i++){
+  array (i)=High
+}  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
