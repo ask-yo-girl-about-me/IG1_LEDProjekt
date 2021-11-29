5eaf86ad3258ebf9364c2f0c208558290e943e8a
gshmeidig <mschmidli@gmx.net>
2021-11-29 15:42:45 +0100

    .

--- 03_Projekt/03_Arduino_Sketches/03_Testing/Vorlage_Switchcase/Switchcase_Test/Switchcase_Test.ino
+++ 03_Projekt/03_Arduino_Sketches/03_Testing/Vorlage_Switchcase/Switchcase_Test/Switchcase_Test.ino
@@ -1,86 +1,66 @@
-int pushButton = 10;
-int pause=100; // 100 Millisekunden Pause bis zur Ansteuerung der nächsten LED.
+int Button = 10;
+//int pause=100; // 100 Millisekunden Pause bis zur Ansteuerung der nächsten LED.
 
 void setup() 
 {
-
+  pinMode(Button, INPUT); //Pin Modus definietion
+  Serial.begin(9600); //Serial Port initalisierung
 }
 
-void loop() 
-  {
-    val1 = digitalRead(pushButton);
-    if (val1 == LOW)
-    {  
-      //delay(50); //50mS
-      val1 = digitalRead(pushButton);
-      if (val1 == HIGH) 
-      {
-        Serial.println(++num);
-      }
-    }
-  
-    val2 = digitalRead(pushButton);
-    if (val2 == LOW)
-    {  
-      //delay(50); //50mS
-      val2 = digitalRead(pushButton);
-      if (val2 == HIGH) 
-      {
-        Serial.println(num=0);
-      }
-    }
-  }
+void loop() {
+    int buttonValue = analogRead(Button)  / 341;  //ausgabe des Button dividiert durch 341 damit 3 mögliche Cases eintreffen
 
-switch(num)
-  {
-    case 1:
-
-      break;
-      
-    case 2:
-    pinMode(0, OUTPUT);
-    digitalWrite(0, HIGH);
-    delay (1000);
-    digitalWrite (0, LOW);
-  pinMode(1, OUTPUT);
-    digitalWrite(1, HIGH);
-    delay (1000);
-    digitalWrite (1, LOW);
-  pinMode(2, OUTPUT);
-    digitalWrite(2, HIGH); 
-    delay (1000); 
-    digitalWrite (2, LOW);
-  pinMode(3, OUTPUT);
-    digitalWrite(3, HIGH);  
-    delay (1000);
-    digitalWrite (3, LOW);
-  pinMode(4, OUTPUT);
-    digitalWrite(4, HIGH);  
-    delay (1000);
-    digitalWrite (4, LOW);
-  pinMode(5, OUTPUT);
-    digitalWrite(5, HIGH); 
-    delay (1000);
-    digitalWrite (5, LOW);// Programm Temp Messung
-      break;
+    //if (buttonValue != lastPotValue)  //prüft ob sich etwas geändert hat
     
-    case 3:
-      pinMode(6, OUTPUT);
-    digitalWrite(6, HIGH);  
-    delay (1000);
-    digitalWrite (6, LOW);
-  pinMode(7, OUTPUT);
-    digitalWrite(7, HIGH);  
-    delay (1000);
-    digitalWrite (7, LOW);
-  pinMode(8, OUTPUT);
-    digitalWrite(8, HIGH);  
-    delay (1000);
-    digitalWrite (8, LOW);
-  pinMode(9, OUTPUT);
-    digitalWrite(9, HIGH);
-    delay (1000);  
-    digitalWrite (9, LOW);// Programm DB Meter
-      break;
-  }
- }
+    switch(buttonValue) {
+      
+        case 0:
+        pinMode(0, OUTPUT);
+        digitalWrite(0, HIGH);
+        delay (1000);
+        digitalWrite (0, LOW);
+         pinMode(1, OUTPUT);
+        digitalWrite(1, HIGH);
+        delay (1000);
+        digitalWrite (1, LOW);
+        pinMode(2, OUTPUT);
+        digitalWrite(2, HIGH); 
+        delay (1000); 
+        digitalWrite (2, LOW);
+          break;
+          
+        case 2:
+        pinMode(3, OUTPUT);
+        digitalWrite(3, HIGH);  
+        delay (1000);
+        digitalWrite (3, LOW);
+        pinMode(4, OUTPUT);
+        digitalWrite(4, HIGH);  
+        delay (1000);
+        digitalWrite (4, LOW);
+        pinMode(5, OUTPUT);
+        digitalWrite(5, HIGH); 
+        delay (1000);
+        digitalWrite (5, LOW);// Programm Temp Messung
+          break;
+        
+        case 3:
+          pinMode(6, OUTPUT);
+        digitalWrite(6, HIGH);  
+        delay (1000);
+        digitalWrite (6, LOW);
+        pinMode(7, OUTPUT);
+        digitalWrite(7, HIGH);  
+        delay (1000);
+        digitalWrite (7, LOW);
+        pinMode(8, OUTPUT);
+        digitalWrite(8, HIGH);  
+        delay (1000);
+        digitalWrite (8, LOW);
+        pinMode(9, OUTPUT);
+        digitalWrite(9, HIGH);
+        delay (1000);  
+        digitalWrite (9, LOW);// Programm DB Meter
+          break;
+      }
+     }
