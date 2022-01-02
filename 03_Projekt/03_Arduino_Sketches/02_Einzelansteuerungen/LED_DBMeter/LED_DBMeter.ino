00de4e42d4a774474287a4c7e29d5d720f7cbb74
leandro.goetzer <leandro@goetzer.ch>
2022-01-02 18:37:45 +0100

    Ablage angepasst

--- 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/LED_DBMeter/LED_DBMeter.ino
+++ 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/LED_DBMeter/LED_DBMeter.ino
@@ -1,29 +1,30 @@
+#define MIC A0
+
+int sig = A0;
+
 void setup() {
-//here is an input for sound sensor
-pinMode(A0, INPUT);
-//here we are setting up all pins as an outputs for LEDs
-for(int z = 0; z < 6; z++){
-pinMode(z, OUTPUT);
+ pinMode(0, OUTPUT);
+ pinMode(1, OUTPUT);
+ pinMode(2, OUTPUT);
+ pinMode(3, OUTPUT);
+ pinMode(4, OUTPUT);
+ pinMode(5, OUTPUT);
+ pinMode(6, OUTPUT);
+
 }
+
+void led() {
+ sig = analogRead(MIC)*250;
+
+ if (sig>60000)  {digitalWrite(0, HIGH);} else {digitalWrite(0, LOW);}
+ if (sig>90000)  {digitalWrite(1, HIGH);} else {digitalWrite(1, LOW);}
+ if (sig>130000) {digitalWrite(2, HIGH);} else {digitalWrite(2, LOW);}
+ if (sig>160000) {digitalWrite(3, HIGH);} else {digitalWrite(3, LOW);}
+ if (sig>190000) {digitalWrite(4, HIGH);} else {digitalWrite(4, LOW);}
+ if (sig>220000) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
+ if (sig>260000) {digitalWrite(6, HIGH);} else {digitalWrite(6, LOW);}
 }
+
 void loop() {
-//here we are storing the volume value
-int volume = analogRead(A0);
-//max value for analog read is 1023 but it must be very very loud to reach this value
-//so I lower it down in map function to 700
-//mapping volume value to make it easier to turn LEDs on
-volume = map(volume, 0, 4000, 0, 10);
-//for loop to turn on or off all LEDs
-//thanks to this loop code for this project is very short
-//we are going through all pins where we have LEDs and checking if the volume is
-//bigger then pin number (that's why we are maping the volume)
-for(int a = 0; a < 6; a++){ 
- if(volume >= a){
- //if it is bigger we can turn on the LED
- digitalWrite(a, HIGH);
- }else{
- //if it is smaller we can turn the LED off
- digitalWrite(a, LOW);
- }
-}
+ led();
 }
--- 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/button/README.md
+++
@@ -1,2 +0,0 @@
-# IG1_LEDProjekt
-Dieses Repository ist für das Modul IG1 für Goetzer und Schmidli
