35926f547acc0ada8ea3d2f4322850768e3c6c1f
gshmeidig <mschmidli@gmx.net>
2022-01-09 19:30:09 +0100

    a

--- 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/LED_DBMeter/LED_DBMeter.ino
+++ 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/LED_DBMeter/LED_DBMeter.ino
@@ -3,6 +3,7 @@
 int sig = A0;
 
 void setup() {
+ Serial.begin(9600);
  pinMode(0, OUTPUT);
  pinMode(1, OUTPUT);
  pinMode(2, OUTPUT);
@@ -14,14 +15,22 @@ void setup() {
 }
 
 void led() {
- sig = analogRead(MIC)*250;
-
- if (sig>60000)  {digitalWrite(0, HIGH);} else {digitalWrite(0, LOW);}
- if (sig>90000)  {digitalWrite(1, HIGH);} else {digitalWrite(1, LOW);}
- if (sig>150000) {digitalWrite(2, HIGH);} else {digitalWrite(2, LOW);}
- if (sig>190000) {digitalWrite(3, HIGH);} else {digitalWrite(3, LOW);}
- if (sig>220000) {digitalWrite(4, HIGH);} else {digitalWrite(4, LOW);}
- if (sig>250000) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
+  // read the sensors values
+  long db_value = 0;
+    for(int i=0; i<32; i++)
+    {
+        db_value += analogRead(A0);
+    }
+    db_value >>= 5;
+    Serial.println(db_value);
+    delay(10);
+     if (db_value>200) {digitalWrite(0, HIGH);} else {digitalWrite(0, LOW);}
+     if (db_value>250) {digitalWrite(1, HIGH);} else {digitalWrite(1, LOW);}
+     if (db_value>300) {digitalWrite(2, HIGH);} else {digitalWrite(2, LOW);}
+     if (db_value>350) {digitalWrite(3, HIGH);} else {digitalWrite(3, LOW);}
+     if (db_value>400) {digitalWrite(4, HIGH);} else {digitalWrite(4, LOW);}
+     if (db_value>500) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
+     if (db_value>600) {digitalWrite(6, HIGH);} else {digitalWrite(6, LOW);}
 }
 
 void loop() {
