1651e3f63562a4e167eae320b3cc96bbd6c32008
leandro.goetzer <leandro@goetzer.ch>
2022-01-11 20:14:44 +0100

    DB Meter Switch

---
+++ 03_Projekt/03_Arduino_Sketches/02_Einzelansteuerungen/Button/ButtonCount/Test_mit_DB_Temp_MS/Test_mit_DB_Temp_LG.ino
@@ -0,0 +1,144 @@
+#include <Arduino_MKRENV.h>
+#include <SPI.h>
+#include <SD.h>
+#define MIC A0
+
+// file object
+File dataFileDB;
+File dataFileTEMP;
+
+const int button = 8;     // button pin
+const int SD_CS_PIN = 4;
+
+// CountButton  //////////////////////////////////////////////////
+int buttonState = 0;         
+int count_value = 0;
+int prestate = 0;
+
+// DBMeter ////////////////////////////////////////////////////
+int sig = A0;
+
+void setup() {
+  pinMode(button, INPUT_PULLUP);      // initialize the pushbutton pin as an input:
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  //pinMode(4, OUTPUT); // Ausgeklammert wegen SD Karte welche auch auf PIN 4 ist
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  
+  Serial.begin(9600);
+  while (!Serial);
+    
+  if (!ENV.begin()) {
+    Serial.println("Failed to initialize MKR ENV shield!");
+    while (1);
+  }
+
+  // init SPI
+  SPI.begin();
+  
+  // init SD card
+  if(!SD.begin(SD_CS_PIN)) {
+    Serial.println("Failed to initialize SD card!");
+    while (1);
+  }
+  
+  // init the logfile for Temperature Values
+  dataFileDB = SD.open("logDB-0000.csv", FILE_WRITE);
+  delay(1000);
+  // init the CSV file with headers
+  dataFileDB.println("db_value,");
+  // close the file
+  dataFileDB.close();
+  delay(100);
+
+  // init the logfile for Temperature Values
+  dataFileTEMP = SD.open("logTemp-0000.csv", FILE_WRITE);
+  delay(1000);
+  // init the CSV file with headers
+  dataFileTEMP.println("temperature,");
+  // close the file
+  dataFileTEMP.close();
+  delay(100);
+  
+}
+
+void loop() {
+
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(button);
+
+  // check if the pushbutton is pressed. If it is, then the buttonState is HIGH:
+  if (buttonState == LOW && prestate == 0) {
+    count_value++;
+    Serial.println(count_value);
+    delay(100);
+    prestate = 1;
+  } else if(buttonState == HIGH) {
+    prestate = 0;
+  }
+
+  if (count_value == 1){
+
+      // init the logfile 
+      dataFileTEMP;
+      delay(10);
+
+     float temperature = ENV.readTemperature();
+
+       // print each of the sensor values
+        
+       Serial.print("Temperature = ");
+       Serial.print(temperature);
+      
+       // print an empty line
+      
+       Serial.println();
+       if (temperature<19.99)  {digitalWrite(6, HIGH);} else {digitalWrite(6, LOW);}
+       if (temperature>20.00)  {digitalWrite(0, HIGH);} else {digitalWrite(0, LOW);}
+       if (temperature>25.00)  {digitalWrite(1, HIGH);} else {digitalWrite(1, LOW);}
+       if (temperature>27.00)  {digitalWrite(2, HIGH);} else {digitalWrite(2, LOW);}
+       if (temperature>29.00) {digitalWrite(3, HIGH);} else {digitalWrite(3, LOW);}
+       if (temperature>31.00) {digitalWrite(4, HIGH);} else {digitalWrite(4, LOW);}
+       if (temperature>35.00) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
+
+       // print each of the sensor values
+       dataFileTEMP.print(temperature);
+       dataFileTEMP.print(",");
+  }
+  if (count_value == 2){
+
+    // init the logfile 
+    dataFileDB;
+    delay(10);
+    
+    // read the sensors values
+    long db_value = 0;
+    for(int i=0; i<32; i++)
+      {
+          db_value += analogRead(A0);
+      }
+      db_value >>= 5;
+      Serial.println(db_value);
+      delay(10);
+       if (db_value>200) {digitalWrite(0, HIGH);} else {digitalWrite(0, LOW);}
+       if (db_value>250) {digitalWrite(1, HIGH);} else {digitalWrite(1, LOW);}
+       if (db_value>300) {digitalWrite(2, HIGH);} else {digitalWrite(2, LOW);}
+       if (db_value>350) {digitalWrite(3, HIGH);} else {digitalWrite(3, LOW);}
+       if (db_value>400) {digitalWrite(4, HIGH);} else {digitalWrite(4, LOW);}
+       if (db_value>500) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
+       if (db_value>600) {digitalWrite(6, HIGH);} else {digitalWrite(6, LOW);}
+
+       // print each of the sensor values
+       dataFileDB.print(db_value);
+       dataFileDB.print(",");
+       
+    buttonState;
+  }
+  if (count_value == 3){
+    count_value =0;
+    buttonState;
+  }
+}
